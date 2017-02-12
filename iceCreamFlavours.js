df3dfc6196c8b372a44e737ec052616893071faa
Leanne <leanneherrndorf@gmail.com>
2017-02-12 05:32:45 +0000

    Adds looping exercises

--- iceCreamFlavours.js
+++
@@ -1,9 +0,0 @@
-var iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
-iceCreamFlavours.push("root beer");
-console.log(iceCreamFlavours);
-
-console.log(iceCreamFlavours[0]);
-
-console.log(iceCreamFlavours[5]);
-
-console.log(iceCreamFlavours.length);
